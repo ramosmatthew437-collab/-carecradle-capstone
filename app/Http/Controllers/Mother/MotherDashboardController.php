6da72180b2b4e5202a7978b25ea2f1a03d49c5b9
Matthew Ramos <ramosmatthew437@gmail.com>
2026-09-10 00:05:12 +0800

    Enhance login page UI and dashboard improvements

--- app/Http/Controllers/Mother/MotherDashboardController.php
+++ app/Http/Controllers/Mother/MotherDashboardController.php
@@ -59,7 +59,11 @@ $nextVaccination = Vaccination::whereHas('infant', function ($query) use ($mothe
     ->orderBy('next_due_date')
     ->first();
 
-    dd($nextVaccination);
+    dd([
+    'vaccine' => $nextVaccination->vaccine_name,
+    'given_date' => $nextVaccination->date_given,
+    'next_due_date' => $nextVaccination->next_due_date,
+]);
 
     return view('mother.dashboard', compact(
     'mother',
